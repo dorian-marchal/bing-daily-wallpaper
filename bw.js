9c9ee11c06e5babd06ec9631ab3d00321ab5998a
dorian-marchal <marchal.dorian@gmail.com>
2015-04-24 22:33:20 +0200

    Get current day's Bing image url

--- bw.js
+++ bw.js
@@ -1,15 +1,25 @@
-#!/usr/bin/env node
-
 'use strict';
 
 var request = require('request');
 
 request('http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1', function (error, response, body) {
 
-    if (!error && response.statusCode == 200) {
+    if (error || response.statusCode !== 200) {
+        throw new Error('Error while accessing the Bing API');
+    }
 
+    try {
         var res = JSON.parse(body);
-        console.log(res);
+
+        if (typeof res.images === 'undefined' || res.images.length === 0) {
+            throw new Error('Bad format for Bing API response');
+        }
+
+        var imageUrl = 'http://bing.com' + res.images[0].url;
+        console.log(imageUrl);
+    }
+    catch (err) {
+        throw err;
     }
 
 });
